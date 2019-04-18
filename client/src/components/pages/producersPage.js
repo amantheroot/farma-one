ca34027672fa14850dd9f3f7500f105fac4477fb
Aman Kumar <amantheroot@gmail.com>
2019-04-18 08:47:55 +0530

    made producers

--- client/src/components/pages/producersPage.js
+++ client/src/components/pages/producersPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 
+import keygen from "../../assets/keygen";
+
 const mapStateToProps = store => {
   return {
     suppliers: store.suppliers
@@ -11,7 +13,15 @@ class toConnectProducersPage extends Component {
   render() {
     return (
       <div className="producers">
-        <h1>Producers</h1>
+        <h1>Our Producers</h1>
+        <div>
+          <ol>
+            {this.props.suppliers.map(prod => <li key={keygen(prod)}><div>
+            <p>Name: {prod.supplier_name}</p>
+            <p>Farm Located At: {prod.supplier_address}</p>
+            </div></li>)}
+          </ol>
+        </div>
       </div>
     );
   }
