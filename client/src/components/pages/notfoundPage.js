e0876a0f793c03b8a33c53e37729d91c260e6c55
Aman Kumar <amantheroot@gmail.com>
2019-04-18 08:54:04 +0530

    not found thank you

--- client/src/components/pages/notfoundPage.js
+++ client/src/components/pages/notfoundPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import {Link} from "react-router-dom";
+
 class NotFoundPage extends Component {
   render() {
     return (
       <div className="notfound">
         <h1>404 Not Found</h1>
+        <p>The Page You Were Looking For Was Not Found.</p>
+        <Link to="/"><button>Go Back To The Site!</button></Link>
       </div>
     );
   }
--- client/src/components/pages/thankyouPage.js
+++ client/src/components/pages/thankyouPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import {Link} from "react-router-dom";
+
 class ThankYouPage extends Component {
   render() {
     return (
       <div className="thankyou">
-        <h1>Thank You</h1>
+        <h1>Order Successfully Placed!</h1>
+        <h2>Thank you for supporting us :)</h2>
+        <h1>GET HEALTHY!</h1>
+        <Link to="/"><button>Go Back To The Site!</button></Link>
       </div>
     );
   }
