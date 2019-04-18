4b930f5935fde82146ac6ff54ed8fca2d5b0810f
Aman Kumar <amantheroot@gmail.com>
2019-04-18 09:14:27 +0530

    added itemsbasket

--- client/src/components/subcomponents/basket.js
+++ client/src/components/subcomponents/basket.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 
-import {changeCartItem, removeCartItem} from "../../actions/cartActions";
-
-import keygen from "../../assets/keygen";
-
 import {Link} from "react-router-dom";
+
+import ItemsBasket from "./itemsBasket";
 
 const mapStateToProps = store => {
   return {
@@ -14,63 +12,19 @@ const mapStateToProps = store => {
   };
 };
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-const images = importAll(require.context('../../assets/images/products', false, /\.(png|jpe?g|svg)$/));
-
 class ToConnectBasket extends Component {
-  qtyInc = (item) => {
-    this.props.dispatch(changeCartItem({product_id: item.product_id, product_qty: item.product_qty + 1}));
-  }
-  qtyDec = (item) => {
-    const qty = item.product_qty;
-    if (qty > 1) {
-      this.props.dispatch(changeCartItem({product_id: item.product_id, product_qty: qty - 1}));
-    } else {
-      this.props.dispatch(removeCartItem(item.product_id));
-    }
-  }
-  itemRemove = (product_id) => {
-    this.props.dispatch(removeCartItem(product_id));
-  }
   render() {
     let subtotal = 0;
-    const cartItems = this.props.cart.map(item => {
+    this.props.cart.forEach(item => {
       const product = this.props.products.find(product => product.product_id === item.product_id);
-      console.log(product);
-      const image = images.find(img => {
-        return Number.parseInt(img.split('/').pop().split(".").shift()) === item.product_id;
-      });
       const productprice = Math.round(product.product_price*item.product_qty*100)/100;
       subtotal += productprice;
-      return (
-      <li key={keygen(item)}>
-        <div>
-          <img src={image} alt={`product_image_${product.product_name}`}/>
-          <div>
-            <span>{product.product_name}</span>
-            <div>
-              <button onClick={() => this.qtyDec(item)}>−</button>
-              <span>{item.product_qty}</span>
-              <button onClick={() => this.qtyInc(item)}>+</button>
-            </div>
-            <span>Rs. {productprice}</span>
-          </div>
-          <button onClick={() => this.itemRemove(item.product_id)}>X</button>
-        </div>
-      </li>
-      );
     });
     return (
       <div className="basket">
         {this.props.cart.length > 0 ? 
           <div>
-            <div>
-              <ul>
-                {cartItems}
-              </ul>
-            </div>
+            <ItemsBasket/>
             <div>
               <div>Sub Total: <span>Rs. {subtotal}</span></div>
               <div>Delivery Charges: <span>**</span></div>
---
+++ client/src/components/subcomponents/itemsBasket.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+import {changeCartItem, removeCartItem} from "../../actions/cartActions";
+
+import keygen from "../../assets/keygen";
+
+const mapStateToProps = store => {
+  return {
+    products: store.products,
+    cart: store.cart
+  };
+};
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+const images = importAll(require.context('../../assets/images/products', false, /\.(png|jpe?g|svg)$/));
+
+class ToConnectItemsBasket extends Component {
+  qtyInc = (item) => {
+    this.props.dispatch(changeCartItem({product_id: item.product_id, product_qty: item.product_qty + 1}));
+  }
+  qtyDec = (item) => {
+    const qty = item.product_qty;
+    if (qty > 1) {
+      this.props.dispatch(changeCartItem({product_id: item.product_id, product_qty: qty - 1}));
+    } else {
+      this.props.dispatch(removeCartItem(item.product_id));
+    }
+  }
+  itemRemove = (product_id) => {
+    this.props.dispatch(removeCartItem(product_id));
+  }
+  render() {
+    const cartItems = this.props.cart.map(item => {
+      const product = this.props.products.find(product => product.product_id === item.product_id);
+      const image = images.find(img => {
+        return Number.parseInt(img.split('/').pop().split(".").shift()) === item.product_id;
+      });
+      const productprice = Math.round(product.product_price*item.product_qty*100)/100;
+      return (
+      <li key={keygen(item)}>
+        <div>
+          <img src={image} alt={`product_image_${product.product_name}`}/>
+          <div>
+            <span>{product.product_name}</span>
+            <div>
+              <button onClick={() => this.qtyDec(item)}>−</button>
+              <span>{item.product_qty}</span>
+              <button onClick={() => this.qtyInc(item)}>+</button>
+            </div>
+            <span>Rs. {productprice}</span>
+          </div>
+          <button onClick={() => this.itemRemove(item.product_id)}>X</button>
+        </div>
+      </li>
+      );
+    });
+    return (
+      <div className="itemsbasket">
+        <ul>
+          {cartItems}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const ItemsBasket = connect(mapStateToProps)(ToConnectItemsBasket);
+
+export default ItemsBasket;
