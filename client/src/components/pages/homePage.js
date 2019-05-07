eebaf104755b06e1b320aa6e06cb3588e6a240af
Aman Kumar <amantheroot@gmail.com>
2019-05-08 05:26:08 +0530

    home

--- client/src/components/pages/homePage.js
+++ client/src/components/pages/homePage.js
@@ -8,40 +8,45 @@ class HomePage extends Component {
   render() {
     return (
       <div className="home">
-        <div className="homebg">
-          <div className="homebg__1">
+        <div>
+          <div className="homeimage__1 homeimage">
             <img src={HomeBG1} alt="home1"/>
-            <div><h1>Get Healthy While Revolutionizing To A New Era.</h1></div>
+            <div>
+              <h1>Get Healthy While Revolutionizing To A New Era.</h1>
+              <button>BUY NOW</button>
+            </div>
           </div>
-          <div className="homebg__2">
+          <div className="home__about">
+            <h1>Who Are We?</h1>
+            <p>We are a non-profit organisation working for upliftment of the current worsened economic scenario of producers of crop and trying to bring organic food into your life the most affordable, accessible and authentic way, all thanks to THE INTERNET.</p>
+          </div>
+        </div>
+        <div>
+          <div className="homeimage__2 homeimage">
             <img src={HomeBG2} alt="home2"/>
             <div><h1>Get Healthy With More Affordability, Accessibility And Authenticity.</h1></div>
           </div>
-          <div className="homebg__3">
-            <img src={HomeBG3} alt="home3"/>
-            <div><h1>Get Healthy While Supporting The Farmers.</h1></div>
-          </div>
-        </div>
-        <section className="home__about">
-          <article>
-            <h1>Who Are We?</h1>
-            <p>We are a non-profit organisation working for upliftment of the current worsened economic scenario of producers of crop and trying to bring organic food into your life the most affordable, accessible and authentic way, all thanks to THE INTERNET.</p>
-          </article>
-          <article>
+          <div className="home__about">
             <h1>The Point Of Farma One</h1>
             <p>The Organic Food Era is seeing its worst condition in which both the consumers and the producers suffer.</p>
             <p>Organic Crops are bought by mediators at prices between <strong>10-22%</strong> of the original Market Selling Price. Not only this but they are selling the produce at a market price <strong>1.5 to 3</strong> times the price of regular vegetables.</p>
             <p><em>This kind of greedy attitude in the industry is worsening the condition for the farmers.</em></p>
             <p>Moreover the accessibility to organic products are very limited unlike the widespread markets over the internet.</p>
-          </article>
-          <article>
+          </div>
+        </div>
+        <div>
+          <div className="homeimage__3 homeimage">
+            <img src={HomeBG3} alt="home3"/>
+            <div><h1>Get Healthy While Supporting The Farmers.</h1></div>
+          </div>
+          <div className="home__about">
             <h1>What Do We Do?</h1>
             <p>Farma One harnesses the power of the internet to solve the problem. By eliminating the need for a mediator, we allow farmers to directly sell the produce to you via this site.</p>
             <p><em>We regulate the prices such that both the farmer and consumer are benefitted, just by eliminating mediator.</em></p>
             <p>Weeding out the mediators will benefit the farmers as well as the consumers as you will get healthy organic food at affordable prices.</p>
             <p>While dealing with authenticity, every customer has the privilage to visit any of our associated farms for inspection.</p>
-          </article>
-        </section>
+          </div>
+        </div>
       </div>
     );
   }
