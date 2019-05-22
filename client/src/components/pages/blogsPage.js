ef453b99a4f3e9b45af06d4ec3bcf341fda6ab55
Aman Kumar <amantheroot@gmail.com>
2019-05-22 18:05:13 +0530

    blogs

--- client/src/components/pages/blogsPage.js
+++ client/src/components/pages/blogsPage.js
@@ -4,7 +4,53 @@ class BlogsPage extends Component {
   render() {
     return (
       <div className="blogs">
-        <h1>Blog</h1>
+        <h1>Blogs</h1>
+        <article>
+          <header>
+            <h2>Changing The Way We See And Eat Food [ The Glorious Wisdom Of Vedas ]</h2>
+          </header>
+          <main>
+            <p className="veda">
+              <em>
+                अन्नमयं हि सोम्य मनः । – From Chhandogyopanishad<br/> 
+                Meaning : Mind is made from anna.
+              </em>
+            </p>
+            <p>Our team truly glorifies this,what we eat has an impact on the way our body organs function,repair themselves,better themselves.All our team see's is Food as divine friend of god ,but not just some material to fill our stomach for that hunger.</p>
+            <p>Eating does not mean merely filling up the stomach to satisfy hunger, rather it is a Yadnya-karma!</p>
+            <p>Food can be a saviour as well as a destroyer. Hence, consume that which is beneficial. Disintegration of food assisted by the mind, saliva and the tongue results in creation of flesh, blood, bones and finally the mind. All this is God’s Divine play. The type of food we consume forms our attitude. Appropriate diet, pleasure, contemplation on God, spiritual practice and  guru’s grace give rise to Anand. God has given us this body to liberate ourselves (from the cycle of birth and death) and hence, it has to be reared on a sattvik [predominant) diet. Though human beings have the intellect to understand, they consume inappropriate diet under the influence of desires, and in the process invite various ailments. Saints have been saying, ‘Eating does not mean merely filling up the stomach to satisfy hunger, rather it is a yadya karma.’ Those who consume food with this attitude are real superior beings. When consumed as ‘Purna–brahman’( in entirety), it gets transformed into Chaitanya (Divine Consciousness) and hence, should always be consumed as such. Chaitanya imparts enthusiasm, happiness; it helps us to be in communion with the soul. Without Chaitanya this world is meaningless. Such an individual dwells under the illusion that Maya (The Great Illusion) is the reality. Hence, surrender unto the guru.Only then will you be able to identify this sagun (Materialised) Maya. When on the path of spiritual practice, by Guru’s grace, it is this very Maya that takes us to Moksha. Hence, at every stage contemplate on the Holy feet of the Guru (Spiritual Master).</p>
+            <p><em>Even the modern science justifies Food is and as medicine.</em></p>
+            <p>Since a long time ago, human has acknowledged the health benefits of foods. However, with the development of chemical drugs and pills, they have forgotten that the food they eat are directly linked to their health. Foods, in facts are the best medicines and therefore we should take into account carefully how we perceive and use them. Firstly, choosing the right diet and the right food to eat can help improve health and body functions. Secondly, using food for health benefits has been a norm and that scientists are looking forward to create foods that can fight diseases. Not surprisingly, there are some similarities and differences in the views on the health benefits of foods between two different generations.</p>
+            <p>BY THE INDUCING OF MILD POISONS[CHEMICAL FERTILIZERS,PESTICDES] FIRST  THE INDIVIDUAL,THEN THE WHOLE COMMUNITY IS TRANSFORMING TOWARDS MORE NEGATIVITY:  increased crime rates,no proper RELATIONSHIP,deterioated thoughts,wildfire of cancers,unthought of illnesses ,many more.</p>
+          </main>
+        </article>
+        <article>
+          <header>
+            <h2>Our Thoughts On Food</h2>
+            <h3>Supproting the farmers</h3>
+          </header>
+          <main>
+            <p>Even from the past the respect and importance given the beloved nature and farmer is immense</p>
+            <p className="veda">
+              शुनं वाहाः शुनं नरः शुनं कृषतु लाङ्गलम् ।<br/>
+              शुनं वरत्रा बध्यन्तां शुनमष्ट्रामुदिङ्गय ॥४॥ 
+            </p>
+            <p>May the Oxen drawing the Plough bring Welfare and Prosperity to all, May the Farmer driving the Oxen bring Welfare and Prosperity to all, May the Plough making Furrows bring Welfare and Prosperity to all.</p>
+            <p>4.2: May the Strap binding the Plough bring Welfare and Prosperity to all, and May the Goad swinging towards the Oxen bring Welfare and Prosperity to all.</p>
+            <p>There is no point in over-exagerrating the impotance of farmers, but there a point we must think towards;how to safeguard and reward them.</p>
+          </main>
+        </article>
+        <section>
+          <h1>Our Farmers</h1>
+          <div>
+            <iframe title="ep50" width="640" height="360" src="https://www.youtube.com/embed/iMivoBBdWdw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p><em>Bhoomiputra Ep. 50</em></p>
+          </div>
+          <div>
+            <iframe title="ep163" width="640" height="360" src="https://www.youtube.com/embed/gQ-TReCjhAI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p><em>Bhoomiputra Ep. 163</em></p>
+          </div>
+        </section>
       </div>
     );
   }
