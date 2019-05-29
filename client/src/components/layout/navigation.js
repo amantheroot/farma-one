590c0d45d7a433118a315861a3477e6a4f356da2
Aman Kumar <amantheroot@gmail.com>
2019-05-29 15:20:20 +0530

    mobile

--- client/src/components/layout/navigation.js
+++ client/src/components/layout/navigation.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import {NavLink} from "react-router-dom";
 
 import Logo from "../../assets/images/logo.svg";
+import LogoShort from "../../assets/images/logo--short.png";
 import Basket from '../subcomponents/basket';
 
 const mapStateToProps = store => {
@@ -12,25 +13,47 @@ const mapStateToProps = store => {
   };
 };
 class ToConnectNavigation extends Component {
+  navToggleClick = e => {
+    let menutoggle = document.getElementsByClassName("nav__menutoggle")[0];
+    let navlinks = document.getElementsByClassName("nav__links")[0];
+
+    if (menutoggle.classList.value.indexOf("toggled") === -1) {
+      menutoggle.classList.add("toggled");
+      navlinks.classList.add("linkstoggled");
+    } else {
+      menutoggle.classList.remove("toggled");
+      navlinks.classList.remove("linkstoggled");
+    }
+  }
+
   render() {
     return (
       <nav className="navigation">
-        <NavLink to="/"><img className="nav__logo" src={Logo} alt="logo" /></NavLink>
-        <ul className="nav__links">
-          <li><NavLink activeClassName="link--active" to="/" exact>About Us</NavLink></li>
-          <li><NavLink activeClassName="link--active" to="/products">Buy Now</NavLink></li>
-          <li><NavLink activeClassName="link--active" to="/blogs">Blogs</NavLink></li>
-          <li><NavLink activeClassName="link--active" to="/ourteam">Our Team</NavLink></li>
-          <li><NavLink activeClassName="link--active" to="/producers">Our Producers</NavLink></li>
-          <li><NavLink activeClassName="link--active" to="/contact">Contact Us</NavLink></li>
-        </ul>
+        <NavLink to="/">
+          <img className="nav__logo" src={Logo} alt="logo" />
+          <img className="nav__logo--short" src={LogoShort} alt="logo--short" />
+        </NavLink>
+        <div className="nav__links--container">
+          <div className="nav__menutoggle" onClick={this.navToggleClick}>
+            <div className="nav__menubar"></div>
+          </div>
+          <ul className="nav__links">
+            <li><NavLink activeClassName="link--active" to="/" exact onClick={this.navToggleClick}>About Us</NavLink></li>
+            <li><NavLink activeClassName="link--active" to="/products" onClick={this.navToggleClick}>Buy Now</NavLink></li>
+            <li><NavLink activeClassName="link--active" to="/blogs" onClick={this.navToggleClick}>Blogs</NavLink></li>
+            <li><NavLink activeClassName="link--active" to="/ourteam" onClick={this.navToggleClick}>Our Team</NavLink></li>
+            <li><NavLink activeClassName="link--active" to="/producers" onClick={this.navToggleClick}>Our Producers</NavLink></li>
+            <li><NavLink activeClassName="link--active" to="/contact" onClick={this.navToggleClick}>Contact Us</NavLink></li>
+          </ul>
+        </div>
         <div className="nav__basket">
           <div className="nav__basket--basket">
             <i className="fa fa-shopping-basket" aria-hidden="true"></i>
-            <div>
+            <div className="nav__basket--info">
               <div>My Basket</div>
               <div>{this.props.cart.length} items</div>
             </div>
+            <div className="nav__basket--infonum">{this.props.cart.length}</div>
           </div>
           <div className="nav__basket--whitebar"></div>
           <Basket/>
